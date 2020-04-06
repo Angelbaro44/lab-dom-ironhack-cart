c77621290fdb65eb073741c2854fecf96ebdd60f
Angel Baro <angelbaro44@gmail.com>
2020-04-06 17:28:21 -0400

    s

--- js/index.js
+++ js/index.js
@@ -1,25 +1,26 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-  var price = document.querySelector(".price > span").innerText;
-  var qty = document.querySelector("table > tbody > tr > td > input[type=number]").value;
-  var sub = document.querySelector("#cart > tbody > tr > td.subtotal > span").innerText = (price * qty);
-  document.querySelector("#total-value > span").innerText = sub
-  
-  console.log(sub)
-  console.log(price)
-  console.log(qty)
+  console.log('Calculating subtotal, yey!', product);
+  var price1 = document.querySelector("#cart > tbody > tr:nth-child(1) > td.price > span").innerText;
+  var price2 = document.querySelector("#cart > tbody > tr:nth-child(2) > td.price > span").innerText;
+  var qty1 = document.querySelector("table > tbody > tr:nth-child(1) > td > input[type=number]").value;
+  var qty2 = document.querySelector("table > tbody > tr:nth-child(2) > td > input[type=number]").value;
+  var sub1 = document.querySelector("#cart > tbody > tr:nth-child(1) > td.subtotal > span").innerText = (price1 * qty1);
+  var sub2 = document.querySelector("#cart > tbody > tr:nth-child(2) > td.subtotal > span").innerText = (price2 * qty2);
 
+  document.querySelector("#total-value > span").innerText = sub1 + sub2;
+  
+  console.log(sub1)
+  console.log(price1)
+  console.log(qty1)
+   console.log(sub2)
+   console.log(price2)
+   console.log(qty2)
 }
 
 
-
-
-
-
-
-
+hello
 
 
 
@@ -27,13 +28,14 @@ function updateSubtotal(product) {
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
   //... your code goes here
-
+var produucts =document.querySelectorAll('tr');
+console.log(produucts)
   // ITERATION 3
   //... your code goes here
 }
